b1fa0f2a4395be22d4483c36dcda1b1c5308d801
archana-mohanty08 <archanamohanty080@gmail.com>
2025-04-02 13:54:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,76 @@
+//import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Textfrom from './components/TextFrom';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+
+  // let site ={
+  //   title: "New Site",
+  //   about: "Company Profile"
+  // } // Company Object 
+
+  const [mode, updateMode] = useState ('light');
+
+  const [alert, updateAlert] = useState (null);
+
+  const viewAlert= (message, type)=>{
+      updateAlert({
+        msg:message,
+        typ:type
+      })
+      setTimeout(()=>{
+        viewAlert (null);
+      },2000);
+  }
+
+  const changeMode = ()=>{
+    if(mode === 'light'){
+      updateMode('dark');
+      document.body.style.backgroundColor='#444444';
+      
+      setTimeout(()=>{
+        document.title = "App - Light mode";
+      },1000);
+      viewAlert("Dark mode enabled", "success");
+    }
+    else{
+      updateMode('light');
+      document.body.style.backgroundColor="white";
+      setTimeout(()=>{
+        document.title = "App - Light mode";
+      },1000);
+      viewAlert("Light mode enabled", "success");
+    }
+  }
+  return (
+    
+    <>
+    <Router>
+      <Navbar title="New Site" aboutNav="About Us" mode={mode} changeMode={changeMode}/> 
+      <Alert alert={alert} />
+      {/* // Site name// */}
+      <div className='container'>
+       
+        <Routes >
+            <Route exact path="/about" element={ <About mode={mode}/>}> </Route>
+
+            <Route exact path="/" element={<Textfrom viewAlert={viewAlert} label="Modify Content" mode={mode}/>}></Route>
+        </Routes>
+      </div>
+      </Router>
+      </>
+
+  );
+}
+
+export default App;
